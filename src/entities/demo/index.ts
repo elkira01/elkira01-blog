22504d1aa1cb1f20cda5424855362d9e1d56c8cb
elkira01 <elkira01@gmail.com>
2026-04-04 21:26:43 +0100

    refactor: add barrel exports for FSD slices

---
+++ src/entities/demo/index.ts
@@ -0,0 +1,3 @@
+export { store, fullName } from './model/store'
+export { makeData } from './model/table-data'
+export type { Person } from './model/table-data'
---
+++ src/features/demo-form/index.ts
@@ -0,0 +1,13 @@
+export { useAppForm } from './model/form'
+export {
+  fieldContext,
+  useFieldContext,
+  formContext,
+  useFormContext,
+} from './model/form-context'
+export {
+  SubscribeButton,
+  TextField,
+  TextArea,
+  Select,
+} from './ui/FormComponents'
